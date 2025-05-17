f245d9b8881e81d28faaaad00026d51b21ec7ab7
mumairkh <umairmuhammad475@gmail.com>
2025-05-17 15:12:02 +0200

    feat: Add insert and _insert methods to TernarySearchTree class

--- ternary_search_tree.py
+++ ternary_search_tree.py
@@ -10,3 +10,25 @@ class TSTNode:
 class TernarySearchTree:
     def __init__(self):
         self.root = None
+
+    def insert(self, word):
+        if not word:
+            return
+        self.root = self._insert(self.root, word, 0)
+
+    def _insert(self, node, word, index):
+        char = word[index]
+
+        if node is None:
+            node = TSTNode(char)
+
+        if char < node.char:
+            node.left = self._insert(node.left, word, index)
+        elif char > node.char:
+            node.right = self._insert(node.right, word, index)
+        else:
+            if index + 1 < len(word):
+                node.eq = self._insert(node.eq, word, index + 1)
+            else:
+                node.is_end = True
+        return node
